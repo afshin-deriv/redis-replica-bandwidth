af9b98dcad456e0e73b22dbcf342a5da438658ef
Afshin Paydar <afshin.paydar@binary.com>
2022-08-25 03:04:09 +0000

    monitoring

--- monitor/redis_metrics.pl
+++ monitor/redis_metrics.pl
@@ -10,9 +10,14 @@ my $redis = RedisDB->new(
 	port => 6381
 );
 
+my $statsd = DataDog::DogStatsd->new(
+	host  => 'datadog',
+	port  => 8125
+);
+
 while(1){
        sleep(3);
        my $total_net_input_bytes = $redis->info->{'total_net_input_bytes'};
-       stats_gauge('redis.total_net_input_bytes', $total_net_input_bytes);
+       $statsd->gauge('redis.total_net_input_bytes', $total_net_input_bytes);
        # print STDOUT "$total_net_input_bytes\n";
 }
