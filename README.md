9dffde5b05ad48bbd979151816bda2d473dcbd8d
Afshin Paydar <afshin.paydar@binary.com>
2022-08-25 02:24:04 +0000

    Add monitor container

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 
 ```sh
+export DD_AGENT_HOST=<DD_AGENT_HOST>
 export DD_API_KEY=<DataDog API Key>
 export REDIS_UPSTREAM_IP=<Redis Master IP Address>
 export REDIS_UPSTREAM_PORT=<Redis Master Port>
---
+++ monitor/redis_metrics.pl
@@ -0,0 +1,18 @@
+use DataDog::DogStatsd::Helper qw(stats_gauge stats_count);
+use RedisDB;
+
+binmode STDOUT, ':encoding(UTF-8)';
+binmode STDERR, ':encoding(UTF-8)';
+STDOUT->autoflush(1);
+
+my $redis = RedisDB->new(
+	host => 'redis',
+	port => 6381
+);
+
+while(1){
+       sleep(3);
+       my $total_net_input_bytes = $redis->info->{'total_net_input_bytes'};
+       stats_gauge('redis.total_net_input_bytes', $total_net_input_bytes);
+       # print STDOUT "$total_net_input_bytes\n";
+}
